d8bfef323848c5218daf25dfa7a83d523dbed6dc
Sanchita Dhareshwar <81695359+sannie-beep@users.noreply.github.com>
2026-04-27 16:33:47 +0800

    Add skeleton for project card component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
+import ProjectCard from "./components/ProjectCard";
+
 export default function Home() {
     return (
         <div>
             <p>Home!</p>
+            <ProjectCard id="1" title="Project 1" description="This is the first project." />
         </div>
         
     )
